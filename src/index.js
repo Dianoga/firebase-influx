047bf5887dec0d0da85d4868934a36e89e8aef3f
Brian Steere <dianoga7@3dgo.net>
2018-09-28 16:26:17 -0500

    Update deps and hopefully optimize firebase listening

--- src/index.js
+++ src/index.js
@@ -1,76 +1,101 @@
-import path from 'path';
-import influx from 'influx';
-import _ from 'lodash';
-
-const firebase = require('firebase');
+const path = require('path');
+const Influx = require('influx');
+const firebase = require('firebase-admin');
 
 const paths = {
 	configDir: path.resolve(__dirname, '../', 'config/')
 };
 
 const config = require(`${paths.configDir}/config`);
+const serviceAccount = require(`${paths.configDir}/firebase-account.json`);
 
 class Manager {
 	constructor() {
 		firebase.initializeApp({
-			databaseURL: 'https://project-2731511947915132034.firebaseio.com/',
-			serviceAccount: `${paths.configDir}/firebase-account.json`
+			databaseURL: config.firebaseUrl,
+			credential: firebase.credential.cert(serviceAccount)
 		});
 
 		this.firebase = firebase.database();
-		this.influx = influx(config.influx);
+		this.influx = new Influx.InfluxDB(config.influx);
+
+		this.handlePower = this.handlePower.bind(this);
+		this.handleDevice = this.handleDevice.bind(this);
+		this.handleDevices = this.handleDevices.bind(this);
+		this.listenToDevice = this.listenToDevice.bind(this);
 	}
 
 	init() {
-		this.firebase.ref('/devices')
-			.on('value', this.handleDevices.bind(this));
+		// Data from Curb
+		this.firebase.ref('/power').on('value', this.handlePower);
 
-		this.firebase.ref('/power')
-			.on('value', this.handlePower.bind(this));
+		const deviceRef = this.firebase.ref('/devices');
+		deviceRef.once('value').then(snapshot => {
+			const data = snapshot.val();
+			Object.keys(data).forEach(this.listenToDevice);
+		});
+
+		deviceRef.on('child_added', this.handleDevice);
+	}
+
+	listenToDevice(id) {
+		this.firebase
+			.ref(`/devices/${id}`)
+			.on('value', snapshot => this.handleDevice(snapshot.val()));
 	}
 
 	handleDevices(snapshot) {
 		const data = snapshot.val();
+		Object.values(data).forEach(this.handleDevice);
+	}
+
+	handleDevice(device) {
 		const tagKeys = ['name', 'id', 'type'];
-		const time = new Date();
+		const tags = {};
+		const fields = {};
 
-		_.each(data, val => {
-			const tags = {};
-			const fields = { time: time };
+		Object.keys(device).forEach(key => {
+			const attr = device[key];
+			const test = Number.parseFloat(attr);
 
-			_.each(val, (attr, key) => {
-				const test = Number.parseFloat(attr);
+			if (tagKeys.indexOf(key) === -1) {
+				fields[key] = isNaN(test) ? attr.toString() : test.valueOf();
+			} else {
+				tags[key] = key === 'type' ? attr.toString() : attr;
+			}
+		});
 
-				if (tagKeys.indexOf(key) === -1) {
-					fields[key] = isNaN(test) ? attr : test.valueOf();
-				} else {
-					tags[key] = key === 'type' ? attr.toString() : attr;
-				}
-			});
+		if (Object.keys(fields).length === 0) {
+			// Nothing to send
+			return;
+		}
 
-			// console.log(fields, tags);
-
-			this.influx.writePoint('devices', fields, tags, err => {
-				if (err) console.error(err);
-			});
+		this.influx.writeMeasurement('devices', [{ fields, tags }]).catch(err => {
+			console.error(err, fields, tags);
 		});
 	}
 
 	handlePower(snapshot) {
 		const data = snapshot.val();
-		const time = new Date();
+		// const time = new Date();
 
 		const tags = {};
-		const fields = {
-			time: time
-		};
+		const fields = {};
 
-		_.each(data, val => {
-			const key = this.nameToField(val.name);
-			fields[key] = Number.parseFloat(val.watts);
+		const circuitTypes = {};
+		Object.values(data).forEach(val => {
+			let key;
+			if (val.label) {
+				key = val.label;
+			} else {
+				if (!circuitTypes[val.circuit_type]) circuitTypes[val.circuit_type] = 0;
+				key = `${val.circuit_type}-${circuitTypes[val.circuit_type]}`;
+				circuitTypes[val.circuit_type]++;
+			}
+			fields[key] = Number.parseFloat(val.w);
 		});
 
-		this.influx.writePoint('power', fields, tags, err => {
+		this.influx.writeMeasurement('power', [{ fields, tags }]).catch(err => {
 			if (err) console.error(err);
 		});
 	}
